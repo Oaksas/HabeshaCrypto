51ccd6d410663d8865cd0050781bea0ee2f64bea
=oaksas <=>
2024-01-01 16:22:04 +0300

    First commit project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <div className="navbar"></div>
+      <div className="main"></div>
+      <div className="footer"></div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd'
+import { Link } from 'react-router-dom'
+import { Homeoutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons'
+function Navbar() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Navbar
