26a0d5900cde6702eea5f5eebbb9002e21d85727
=oaksas <=>
2024-01-02 10:31:11 +0300

    Add cryptocurrencies list

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -1,36 +1,15 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
-import BaseUrl from 'antd/es/typography/Base';
-import { createRequest } from '../utils';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { BaseUrl, createRequest } from '../utils';
 export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
     baseQuery: fetchBaseQuery({ baseUrl: BaseUrl }),
     endpoints: (builder) => ({
         getCryptos: builder.query({
-            query: () => createRequest('/exchanges'),
+            query: () => createRequest('/coins'),
         }),
     }),
 });
 
+export const { useGetCryptosQuery } = cryptoApi;
 
-// const options = {
-//   method: 'GET',
-//   url: 'https://coinranking1.p.rapidapi.com/exchanges',
-//   params: {
-//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//     limit: '50',
-//     offset: '0',
-//     orderBy: '24hVolume',
-//     orderDirection: 'desc'
-//   },
-//   headers: {
-//     'X-RapidAPI-Key': 'bb7c54c949mshf76f9cd4b889e90p1625f5jsnaf1ff6eef258',
-//     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-//   }
-// };
 
-// try {
-// 	const response = await axios.request(options);
-// 	console.log(response.data);
-// } catch (error) {
-// 	console.error(error);
-// }
