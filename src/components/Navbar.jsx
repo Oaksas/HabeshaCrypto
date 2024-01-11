74cc345613fc80d8f82a681f1445ee038c1fb989
=oaksas <=>
2024-01-11 15:28:28 +0300

    Add responsive menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd'
 import { Link } from 'react-router-dom'
 import { MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined, HomeOutlined } from '@ant-design/icons'
 import cryptoicon from '../assets/images/cryptocurrency.png'
 import { MenuList } from '../utils'
+
 function Navbar() {
+    const [activeMenu, setActiveMenu] = useState(true)
+    const [screenSize, setScreenSize] = useState(null)
+
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth)
+        window.addEventListener('resize', handleResize)
+        handleResize()
+        return () => window.removeEventListener('resize', handleResize)
+
+    }, [])
+
+    useEffect(() => {
+        if (screenSize < 768) {
+            setActiveMenu(false)
+        } else {
+            setActiveMenu(true)
+        }
+    }, [screenSize])
+
+
     return (
         <div className='nav-container'>
             <div className='logo-container'>
